cc0e071067efcf5328a401993a8fd50821a3dc56
Helerin Heinsalu <helehe@ttu.ee>
2025-03-03 17:45:39 +0200

    inital base, dal, domain

---
+++ home-project/production-workflow/App.Domain/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Base.Domain;
+
+namespace App.Domain;
+
+public class Customer : BaseEntity
+{
+    [MaxLength(128)]
+    public string Name { get; set; } = default!;
+    [MaxLength(128)]
+    public string Email { get; set; } = default!;
+    [MaxLength(20)]
+    public string Phone { get; set; } = default!;
+    [MaxLength(255)]
+    public string Address { get; set; } = default!;
+    [MaxLength(255)]
+    public string? AdditionalInfo { get; set; } = default!;
+    
+    public ICollection<Order>? Orders { get; set; }
+    
+    public ICollection<Shipment>? Shipments { get; set; }
+}
